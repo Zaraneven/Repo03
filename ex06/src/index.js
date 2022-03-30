f1a1371aaeea5bdae62666737780dde2924ba446
neven kusturica <neven.kusturica@gmail.com>
2022-03-30 22:48:17 +0200

    Added ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+var x = 7;
+var y = 12;
+var z = "My name is";
+
+var x = x + 2;
+var y = y + 4;
+var z = z + "John!";
+
+return [x,y,z];
+
+    
+    
+}
+
+console.log(main());
+
+module.exports = main;
